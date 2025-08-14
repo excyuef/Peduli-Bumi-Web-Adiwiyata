4aab2f7718dd49be59b2b75b468a6396cb6eeb94
Akbar Afandi <greatemperorvoid@gmail.com>
2025-08-14 10:10:56 +0700

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -2,6 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const bar = document.getElementById("navBar")
   const menu = document.getElementById("menuNavigasi")
   const exit = document.getElementById("xNavigasi")
+  const home = document.getElementById("navhome")
+  const tipsaksi = document.getElementById("navtisi")
+  const galeri = document.getElementById("navgaleri")
+  const tim = document.getElementById("mavtim")
   
   bar.addEventListener("click", () => {
     menu.classList.remove("ilang")
@@ -11,5 +15,21 @@ document.addEventListener("DOMContentLoaded", () => {
   exit.addEventListener("click", () => {
     menu.classList.remove("muncul")
     menu.classList.add("ilang")
+  })
+  
+  home.addEventListener("click", () => {
+    window.location.assign("../index.html")
+  })
+  
+  tipsaksi.addEventListener("click", () => {
+    window.location.assign("../tips-aksi.html")
+  })
+  
+  galeri.addEventListener("click", () => {
+    window.location.assign("../gallery.html")
+  })
+  
+  tim.addEventListener("click", () => {
+    window.location.assign("../ourteaam.html")
   })
 })
